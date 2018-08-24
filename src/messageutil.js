ac29e6737bb985e193ec14397723650b68e101a5
Koutarou Tanaka <from.kyushu.island@gmail.com>
2018-08-24 18:19:49 +0900

    add 'encodeRate' and 'parseRate' function

--- src/messageutil.js
+++ src/messageutil.js
@@ -5,6 +5,9 @@ const {
   Buffer
 } = require('safe-buffer')
 const {
+  BigNumber
+} = require('bignumber.js')
+const {
   OP_RETURN_BYTES,
   OPS
 } = require('./const')
@@ -28,8 +31,37 @@ function isValidMessage (message) {
   return buf.length <= OP_RETURN_BYTES
 }
 
+function encodeRate (amountType, fiatRate) {
+  return encode(`v1.0.0|RATE|${amountType}|${fiatRate}`)
+}
+
+function parseRate (message) {
+  let version = message.match(/^(v\d+\.\d+\.\d+)\|RATE/)
+  if (!version || !version[1]) {
+    return null
+  }
+
+  switch (version[1]) {
+    case 'v1.0.0':
+      return parseRateV1(message)
+    default:
+      return null
+  }
+}
+
+function parseRateV1 (message) {
+  let sp = message.split('|')
+  console.log(sp)
+  return {
+    rate: new BigNumber(sp[3]).toNumber(),
+    currency: sp[2]
+  }
+}
+
 module.exports = {
   encode: encode,
   decode: decode,
+  encodeRate: encodeRate,
+  parseRate: parseRate,
   isValidMessage: isValidMessage
 }
