e221943fa2d938b7f509c949e13bec0b11c1a73d
Koutarou Tanaka <from.kyushu.island@gmail.com>
2019-01-30 00:17:26 +0900

    add comment

--- src/messageutil.js
+++ src/messageutil.js
@@ -12,10 +12,12 @@ const {
   OPS
 } = require('./const')
 
+// Encode message string to OP_RETURN hex-data
 function encode (message) {
   return script.compile([OPS.OP_RETURN, Buffer.from(message, 'utf8')])
 }
 
+// Decode OP_RETURN hex-data to message string
 function decode (hex) {
   let decompiled = script.decompile(Buffer.from(hex, 'hex'))
   let index = decompiled.indexOf(OPS.OP_RETURN)
@@ -26,15 +28,18 @@ function decode (hex) {
   }
 }
 
+// Validate message
 function isValidMessage (message) {
   let buf = Buffer.from(message, 'utf8')
   return buf.length <= OP_RETURN_BYTES
 }
 
+// Encode rate
 function encodeRate (amountType, fiatRate) {
   return encode(`v1.0.0|RATE|${amountType}|${fiatRate}`)
 }
 
+// Parse rate message
 function parseRate (message) {
   let version = message.match(/^(v\d+\.\d+\.\d+)\|RATE/)
   if (!version || !version[1]) {
@@ -49,6 +54,7 @@ function parseRate (message) {
   }
 }
 
+// Parse rate message (v1)
 function parseRateV1 (message) {
   let sp = message.split('|')
   return {
@@ -58,9 +64,40 @@ function parseRateV1 (message) {
 }
 
 module.exports = {
+  /**
+   * Encode message string to OP_RETURN hex-data
+   *
+   * @param {string} message - message string
+   * @return {Buffer} encoded message
+   */
   encode: encode,
+  /**
+   * Decode OP_RETURN hex-data to message string
+   *
+   * @param {string} hex - hex-encoded message
+   * @return {string} decoded message string
+   */
   decode: decode,
+  /**
+   * Encode rate
+   *
+   * @param {string} amountType - amount type (btc, jpy, etc...)
+   * @param {number} fiatRate - rate
+   * @return {Buffer} encoded message
+   */
   encodeRate: encodeRate,
+  /**
+   * Parse rate message
+   *
+   * @param {string} message - rate message
+   * @return {object} parsed rate object
+   */
   parseRate: parseRate,
+  /**
+   * Validate message
+   *
+   * @param {string} message - message string
+   * @return {bool} validation result
+   */
   isValidMessage: isValidMessage
 }
